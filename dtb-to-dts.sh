5601e4975e6097076cd6d36642a4bec8fee969e9
chhapil <ru2raj@gmail.com>
2018-05-19 20:33:53 +0530

    Added naming logic for dts

--- dtb-to-dts.sh
+++ dtb-to-dts.sh
@@ -13,4 +13,16 @@ for i in `find . -type f \( -iname \*.rej \
         #echo Base Dir "$d";
         
        ./dtc -I dtb -O dts -o ./dts/$name.dts $i     
+       
+       t2=`grep 'model = "Q' ./dts/$name.dts | sed 's/.\{2\}$//' | sed -r 's/^.{38}//' | sed -e 's/\s+//g'| sed -e 's/\s/-/g'`
+       
+       
+        echo Chipset Model "$t2"  ;
+        
+        ttt4=`grep 'qcom,board-id =' ./dts/$name.dts  | sed 's/.\{6\}$//' | sed -r 's/^.{18}//'`
+       
+        echo Board Id: "$ttt4" ;
+        
+
+        mv ./dts/$name.dts ./dts/"$t2"-"$ttt4".dts
 done
